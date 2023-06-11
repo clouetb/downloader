6afdb95e653d392d8ecb43b9109812a10f586ec5
Benoît Clouet <benoit.clouet@gmail.com>
2023-06-11 23:27:05 +0200

    Misc. adjustments

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
 Redmine::Plugin.register :downloader do
-  name 'Downloader plugin'
+  name 'Issue\'s attachments downloader plugin'
   author 'Benoît Clouet'
   description 'This plugin allows download of all attachments of the issues of a project'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/clouetb/downloader'
+  author_url 'https://github.com/clouetb'
   # caption = I18n.t 'label_download_menu'
   permission :downloads, { downloads: [:index, :download] }, public: true
-  menu :project_menu, :downloads, {controller: 'downloads', action: 'index'}, caption: 'Download', after: :activity, param: :project_id
+  menu :project_menu, :downloads, {controller: 'downloads', action: 'index'}, caption: 'Download', after: :issues, param: :project_id
   settings :default => { 'attachments_limit' => 10 }, :partial => 'settings/attachments_limit'
 end
